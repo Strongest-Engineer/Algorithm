34bf9af58756c80c6bbd830e38b6c79e721e5b31
yuseonjun <seonjun0906@gmail.com>
2024-03-21 15:21:22 +0900

    feat : Sort - 소트인사이드

---
+++ Sort/BOJ_1427_yuseonjun.java
@@ -0,0 +1,18 @@
+package Sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_1427_yuseonjun {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] arr = sc.nextLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
